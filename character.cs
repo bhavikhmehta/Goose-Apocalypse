352b834cad1e35119e3aef99cd65306cda402f56
lukeoulahen <lukeoulahen@gmail.com>
2014-11-01 20:38:07 -0400

    bassic character class functuonality

--- character.cs
+++ character.cs
@@ -3,5 +3,47 @@ using System.IO;
 
 class character
 {
-	//fields include name, health, morality etc.
+	public string name;
+	public int morality;
+	//Resistor currentResistor;
+	public int health;
+
+	public character( string name )
+	{
+		this.name = name;
+		this.morality = 0;
+		this.health = 100;
+
+		//finish resistor contructor later
+		//this.currentResistor = new Resistor( ) 
+	}
+	public string Name
+	{
+		get
+		{
+			return name;
+		}
+	}
+	public int Morality
+	{
+		get
+		{
+			return morality;
+		}
+		set
+		{
+			this.morality = value;
+		}
+	}
+	public int Health
+	{
+		get
+		{
+			return health;
+		}
+		set
+		{
+			this.health = value;
+		}
+	}
 }
