5ab18a95901781c9250adc32f3904ea3722c42b5
lukeoulahen <lukeoulahen@gmail.com>
2014-11-02 01:53:49 -0500

    initial combat impemantation

---
+++ combat.cs
@@ -0,0 +1,34 @@
+using System;
+
+class Combat
+{	
+	Character c;
+	int gooseDamage = 1;
+	int gooseHealth = 3;
+	public Combat( Character c )
+	{
+		this.c = c;
+		while( ! (c.Health <= 0 || gooseHealth <= 0) )
+		{
+			Display();
+			c.Health = c.Health - gooseDamage;
+			gooseHealth = gooseHealth - (int) c.Resistance;
+		}
+	}
+
+	public void Display()
+	{
+		Console.WriteLine();
+		Console.WriteLine( "gooseHealth {0}", this.gooseHealth);
+		Console.WriteLine( "playerHealth {0}", this.c.Health);
+	}
+}
+
+static class Program
+{
+	static void Main()
+	{	
+		Character c = new Character( "john" );
+		Combat n = new Combat(c);
+	}
+}
