6de56056e8a4f063b004918d5cc3f5bc12e66d15
Bhavik <bhav40_m@hotmail.com>
2014-11-01 21:53:49 -0400

    Change in resistor class

--- resistor.cs
+++ resistor.cs
@@ -1,6 +1,65 @@
 using System;
 
-class resistor
+
+class Resistor
 {
-	
+    double tolerance; 
+    double resistance;
+
+
+    public Resistor (double resistance, double tolerance)
+    {
+        this.tolerance = tolerance;
+        this.resistance = resistance;
+    }  
+
+    //cresistance is the current weapon of the player
+    //optresistance is the resistor the use will potentionally pick up
+    public void Change (double optresistance, double opttolerance)
+    {
+
+        Random value = new Random();
+        int choose = value.Next(0,2);
+
+        //parallel option
+        if (choose == 0)
+        {
+            this.resistance = this.resistance - (1 / ((1 / this.resistance) + (1 / optresistance)));
+        }
+
+        //series option
+        if (choose == 1)
+        {
+            this.resistance = this.resistance + optresistance;
+        }
+
+        if (opttolerance < this.tolerance)
+        {
+            this.tolerance = opttolerance;
+        }
+
+    }
+
+    //swapping for a new resistor
+    public void Swap (double tolerance, double resistance)
+    {
+        this.resistance = resistance;
+        this.tolerance = tolerance;
+    }
+
+    public double Tolerance
+    {
+        get 
+        {
+            return this.tolerance;
+        }
+    }
+
+    public double Resistance
+    {
+        get
+        {
+            return this.resistance;
+        }
+    }
 }
